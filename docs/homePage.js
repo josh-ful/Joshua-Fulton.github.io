4bdbc1d7c56cfc6052926317f2201327c715f941
josh_fu <jwfulton221@gmail.com>
2025-06-27 13:42:03 -0400

    Added new files used for testing

---
+++ docs/homePage.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('img');
+
+    const abButton = this.querySelector('#abButton');
+    const information = this.querySelector('#information');
+
+    let visibility = false;
+    function noDragging() {
+        for(var i = 0; i < images.length; i++) {
+            images[i].setAttribute('draggable', false);
+            images[i].addEventListener('dragstart', function(event) {
+                event.preventDefault();
+            })
+        }
+    }
+
+
+
+    function newPage (pageName) {
+        window.location.href = pageName;
+        console.log(pageName);
+    }
+    abButton.addEventListener('click', () => newPage("aboutMe.html"));
+     
+    abButton.addEventListener('mousedown', () => {
+        abButton.classList.add('active');
+    });
+
+    abButton.addEventListener('mouseup', () => {
+        abButton.classList.remove('active');
+    });
+
+    noDragging();
+
+
+
+
+});
